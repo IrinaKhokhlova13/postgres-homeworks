43507f80458b982c91b18c84afb3f77cc06ff721
IrinaKhokhlova13 <irina.khokhlova.50@gmail.com>
2024-04-10 18:17:39 +0300

    Итоговый вариант домашнего задания 1 по БД

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,41 @@
 -- SQL-команды для создания таблиц
+
+-- Создала через командную строку базу данных north
+CREATE DATABASE north
+
+
+
+-- Создать три таблицы, используя query tool в pgAdmin:  employees, customers и orders
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name text,
+	last_name text,
+	title text NOT NULL,
+	birth_date date,
+	notes text
+);
+
+
+CREATE TABLE customers
+(
+	customer_id varchar(10) PRIMARY KEY,
+	company_name text,
+	contact_name text
+);
+
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(10) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city text
+);
+
+
+-- Зайти в pgAdmin и убедиться, что данные в таблицах есть
+SELECT * FROM customers
+SELECT * FROM employees
+SELECT * FROM orders
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,65 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+from pathlib import Path
+from config import ROOT_DIR
+import csv
+
+
+#чтение из csv файла
+def rm_file(name_file):
+    file_path = Path(ROOT_DIR, f'north_data/{name_file}')
+    with open(file_path, encoding='utf-8') as f:
+        reader_dict = []
+        reader = csv.reader(f)
+        for row in reader:
+            reader_dict.append(row)
+    reader_dict.pop(0)
+    return reader_dict
+
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345"
+)
+try:
+    with conn:
+        with conn.cursor() as cur:
+            for customer in rm_file(('customers_data.csv')):
+                cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", (customer[0], customer[1], customer[2]))
+                cur.execute("SELECT * FROM customers")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+finally:
+    conn.close()
+
+
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            for employee in rm_file(('employees_data.csv')):
+                cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", (employee[0], employee[1], employee[2], employee[3], employee[4], employee[5]))
+                cur.execute("SELECT * FROM employees")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+finally:
+    conn.close()
+
+
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            for order in rm_file(('orders_data.csv')):
+                cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", (order[0], order[1], order[2], order[3], order[4]))
+                cur.execute("SELECT * FROM orders")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+finally:
+    conn.close()
